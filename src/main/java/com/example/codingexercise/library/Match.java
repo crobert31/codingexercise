681c25cd8607c55bd48fc3b6fe858f6268eaf239
Robert Corluka <robert.corluka-ext@hpe.com>
2025-02-02 15:56:12 +0100

    added match paramteres

--- src/main/java/com/example/codingexercise/library/Match.java
+++ src/main/java/com/example/codingexercise/library/Match.java
@@ -1,4 +1,35 @@
 package com.example.codingexercise.library;
 
 public class Match {
+    private final String homeTeam;
+    private final String awayTeam;
+    private int homeScore;
+    private int awayScore;
+    private final long startTime;
+
+    public Match(String homeTeam, String awayTeam) {
+        this.homeTeam = homeTeam;
+        this.awayTeam = awayTeam;
+        this.homeScore = 0;
+        this.awayScore = 0;
+        this.startTime = System.nanoTime();
+    }
+
+    public void updateScore(int homeScore, int awayScore) {
+        this.homeScore = homeScore;
+        this.awayScore = awayScore;
+    }
+
+    public int getTotalScore() {
+        return homeScore + awayScore;
+    }
+
+    public long getStartTime() {
+        return startTime;
+    }
+    @Override
+    public String toString() {
+        return homeTeam + " " + homeScore + " - " + awayTeam + " " + awayScore;
+    }
+
 }
