860e3a5e678732e003061bc315635163eaef2bdc
Robert Corluka <robert.corluka-ext@hpe.com>
2025-02-02 11:25:09 +0100

    Initial commit
    and tests skeleton

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# codingexercise
+# Coding Exercise - Live Odds Services 
+
+## Assumption 1: 
+We will use Kafka because its useful for real-time event streaming, ensuring scalability, fault tolerance, message ordering all that we need for this exercise.
---
+++ src/test/java/com/example/codingexercise/LiveoddsApplicationTests.java
@@ -0,0 +1,52 @@
+package com.example.codingexercise;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@ExtendWith(MockitoExtension.class)
+class LiveoddsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    void startMatchTest() {
+
+    }
+
+    @Test
+    void endMatchTest() {
+
+    }
+
+    @Test
+    void updateScoreTest() {
+
+    }
+
+    @Test
+    void startMatchWithSameTeamsTest() {
+
+    }
+
+    @Test
+    void endMatchNonExistentTeamsTest() {
+
+    }
+    @Test
+    void negativeScoreUpdate() {
+
+    }
+
+    @Test
+    void matchesSummaryOrderTest() {
+
+    }
+
+
+
+}
