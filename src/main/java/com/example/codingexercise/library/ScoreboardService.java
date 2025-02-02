f3855f9b5ad661c51e1e2f46764b7bb33bfa55f9
Robert Corluka <robert.corluka-ext@hpe.com>
2025-02-02 16:11:41 +0100

    prepared kafka skeleton

--- src/main/java/com/example/codingexercise/library/ScoreboardService.java
+++ src/main/java/com/example/codingexercise/library/ScoreboardService.java
@@ -1,4 +1,29 @@
 package com.example.codingexercise.library;
 
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Service
 public class ScoreboardService {
+    private final Map<String, Match> liveMatches = new ConcurrentHashMap<>();
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public ScoreboardService(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendMessage(String topic, String message) {
+        kafkaTemplate.send(topic, message);
+    }
+
+    @KafkaListener(topics = "matches", groupId = "scoreboard-group")
+    public void listen(String message) {
+        System.out.println("Kafka Event: " + message);
+    }
+
+
 }
